ea815a3984a23c0c9680628cdb33e99e1ea0253a
Daniel Okello <handsdani@gmail.com>
2025-02-26 17:10:01 +0300

    Added an endpoint for matching face (Takes form data)

--- facematch.py
+++ facematch.py
@@ -1,8 +1,8 @@
 import cv2
 import base64
-import insightface
 import logging
 import numpy as np
+import json
 from insightface.app import FaceAnalysis
 
 logging.basicConfig(level=logging.DEBUG)
@@ -13,8 +13,10 @@ class FaceMatch:
         self.idcardpath = idcardpath
         self.app = FaceAnalysis(providers=['CUDAExecutionProvider','CPUExecutionProvider'],det_thresh=0.5)
         self.THRESHOLD=0.5
+
     def prepare_model(self):
         self.app.prepare(ctx_id=0,det_size=(640,640))
+
     def _draw_detections(self,img):
         img = cv2.imread(img)
         faces = self.app.get(img)
@@ -28,9 +30,9 @@ class FaceMatch:
 
         _,encoded_img = cv2.imencode('.jpg', img)
         base64_img = base64.b64encode(encoded_img).decode('utf-8')
-
         return base64_img
-    def get_face_embeddings(self,img):
+
+    def _get_face_embeddings(self,img):
         img = cv2.imread(img)
         faces = self.app.get(img)
         if len(faces) == 0:
@@ -41,8 +43,34 @@ class FaceMatch:
             key=lambda face: (face.bbox.shape[2] - face.bbox.shape[0] - face.bbox.shape[1]),
             reverse=True
         )[0]
-
         return face
+
     def _cosine_similarity(self,embedding1,embedding2):
         return np.dot(embedding1,embedding2) / (np.linalg.norm(embedding1)*np.linalg.norm(embedding2))
+
+    def match(self,img1,img2):
+        self.selfiepath = img1
+        self.idcardpath = img2
+
+        self.prepare_model(self)
+        selfie_base64 = self._draw_detections(self,self.selfiepath)
+        idcard_base64 = self._draw_detections(self,self.idcardpath)
+
+        selfie_embedding = self._get_face_embeddings(self.selfiepath)
+        idcard_embedding = self._get_face_embeddings(self.idcardpath)
+
+        similarity = self._cosine_similarity(selfie_embedding,idcard_embedding)
+
+        # calculate similarity based on a threshold
+        ismatch = similarity >= self.THRESHOLD
+        return json.dumps(
+            {
+                ismatch,
+                similarity,
+                selfie_base64,
+                idcard_base64
+            })
+
+
+
 
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,File
+from facematch import FaceMatch
+import logging
+from pydantic import BaseModel
 
+logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
+facematch = FaceMatch()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+image_extensions = ["jpg", "jpeg", "png"]
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/face-match")
+async def facematch(img1: File(), img2: File()):
+    try:
+        if img1 is None or img2 is None:
+            raise HTTPException(status_code=400, detail="img1 and img2 are required")
+        response = facematch.faceMatch(img1, img2)
+    except Exception as e:
+        logging.error(e)
+        raise HTTPException(status_code=400, detail=str(e))
+    return response
