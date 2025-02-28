8bb8a67b47bab89410f4df6cedbd16707458ea58
Daniel Okello <handsdani@gmail.com>
2025-02-28 16:31:04 +0300

    Added an endpoint for matching face (Takes form data)

--- facematch.py
+++ facematch.py
@@ -3,47 +3,59 @@ import base64
 import logging
 import numpy as np
 import json
+from PIL import Image
 from insightface.app import FaceAnalysis
 
 logging.basicConfig(level=logging.DEBUG)
 
 class FaceMatch:
-    def __init__(self, selfiepath: str, idcardpath: str ):
+    def __init__(self, selfiepath, idcardpath):
         self.selfiepath = selfiepath
         self.idcardpath = idcardpath
-        self.app = FaceAnalysis(providers=['CUDAExecutionProvider','CPUExecutionProvider'],det_thresh=0.5)
         self.THRESHOLD=0.5
-
-    def prepare_model(self):
-        self.app.prepare(ctx_id=0,det_size=(640,640))
+        self.app = FaceAnalysis(providers=['CUDAExecutionProvider','CPUExecutionProvider'],
+                                det_thresh=0.5)
 
     def _draw_detections(self,img):
-        img = cv2.imread(img)
-        faces = self.app.get(img)
-        if len(faces) == 0:
-            logging.info("No face detected")
-            return
-        for face in faces:
-            bbox = face.bbox.astype(np.int)
+        try:
+            img_array = np.asarray(bytearray(img), dtype="uint8")
+            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+            logging.info(f'read file {img}')
+            self.app.prepare(ctx_id=0, det_size=(640, 640))
+            faces = self.app.get(img)
+            logging.info(f'read faces {len(faces)}')
 
-        cv2.rectangle(img,(bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,0,0), 2)
+            if len(faces) == 0:
+                logging.info("No face detected")
+                return
+            for face in faces:
+                bbox = face.bbox.astype('int')
 
-        _,encoded_img = cv2.imencode('.jpg', img)
-        base64_img = base64.b64encode(encoded_img).decode('utf-8')
-        return base64_img
+            cv2.rectangle(img,(bbox[0], bbox[1]), (bbox[2], bbox[3]), (121,226,219), 2)
+
+            _,encoded_img = cv2.imencode('.jpg', img)
+            base64_img = base64.b64encode(encoded_img).decode('utf-8')
+
+            return base64_img
+        except Exception as e:
+            logging.error(e)
 
     def _get_face_embeddings(self,img):
-        img = cv2.imread(img)
+        img_array = np.asarray(bytearray(img), dtype="uint8")
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        logging.info(f'read file {img}')
+        self.app.prepare(ctx_id=0, det_size=(640, 640))
         faces = self.app.get(img)
         if len(faces) == 0:
             logging.info("No face detected")
             return
         face = sorted(
             faces,
-            key=lambda face: (face.bbox.shape[2] - face.bbox.shape[0] - face.bbox.shape[1]),
+            key=lambda face: (face.bbox[2] - face.bbox[0] - face.bbox[1]),
             reverse=True
         )[0]
-        return face
+        logging.info('face :',len(face))
+        return face.embedding
 
     def _cosine_similarity(self,embedding1,embedding2):
         return np.dot(embedding1,embedding2) / (np.linalg.norm(embedding1)*np.linalg.norm(embedding2))
@@ -52,25 +64,26 @@ class FaceMatch:
         self.selfiepath = img1
         self.idcardpath = img2
 
-        self.prepare_model(self)
-        selfie_base64 = self._draw_detections(self,self.selfiepath)
-        idcard_base64 = self._draw_detections(self,self.idcardpath)
+        selfie_base64 = self._draw_detections(self.selfiepath)
+        idcard_base64 = self._draw_detections(self.idcardpath)
 
         selfie_embedding = self._get_face_embeddings(self.selfiepath)
         idcard_embedding = self._get_face_embeddings(self.idcardpath)
+
+        logging.info(f'selfie embedding: {selfie_embedding}')
+        logging.info(f'idcard embedding: {idcard_embedding}')
 
         similarity = self._cosine_similarity(selfie_embedding,idcard_embedding)
 
         # calculate similarity based on a threshold
         ismatch = similarity >= self.THRESHOLD
-        return json.dumps(
-            {
-                ismatch,
-                similarity,
-                selfie_base64,
-                idcard_base64
-            })
 
+        return {
+            "similarity": float(similarity),
+            "ismatch": bool(ismatch),
+            "selfie": selfie_base64,
+            "idcard": idcard_base64
+        }
 
 
 
--- main.py
+++ main.py
@@ -1,22 +1,63 @@
-from fastapi import FastAPI,HTTPException,File
-from facematch import FaceMatch
 import logging
-from pydantic import BaseModel
+import filetype
+import PIL.Image as Image
+from io import BytesIO
+from typing import Annotated,IO
+from facematch import FaceMatch
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI,HTTPException,File,status
+from scalar_fastapi import get_scalar_api_reference
 
 logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
-facematch = FaceMatch()
-
-image_extensions = ["jpg", "jpeg", "png"]
-
+@app.get("/", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title=app.title,
+    )
 @app.post("/face-match")
-async def facematch(img1: File(), img2: File()):
+async def face_match(
+        img1:Annotated[bytes, File()],
+        img2:Annotated[bytes, File()]
+):
     try:
         if img1 is None or img2 is None:
             raise HTTPException(status_code=400, detail="img1 and img2 are required")
-        response = facematch.faceMatch(img1, img2)
+
+        # convert bytes to Image for validation
+        image_1 = bytestoimage(img1)
+        image_2 = bytestoimage(img2)
+
+        facematch = FaceMatch(img1, img2)
+        response = facematch.match(img1, img2)
+
+        return JSONResponse(
+            content=response,
+            media_type="application/json",
+            status_code=200
+        )
     except Exception as e:
         logging.error(e)
-        raise HTTPException(status_code=400, detail=str(e))
-    return response
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Error: {str(e)}'
+        )
+
+def bytestoimage(image_bytes):
+    image = Image.open(BytesIO(image_bytes))
+    logging.info(f'converted bytes to image: {image.filename}')
+    return image
+
+def validate_file_size_type(file: IO):
+    FILE_SIZE=2097152 #2mb
+    accepted_types = ["image/png", "image/jpeg", "image/jpg", "png","jpeg", "jpg"]
+
+    file_info = filetype.guess(file)
+    if file_info is None:
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail="Unable to determine the file type"
+        )
+    detected_content_type = file_info.extension.lower()
